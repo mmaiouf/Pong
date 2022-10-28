5eac384aaaa433a970d178a47dbceaec0ffae066
Mouaad MAIOUF <mouaad69@hotmail.fr>
2022-10-28 19:58:59 +0200

    append the 3 main LOVE functions

---
+++ Project/main.lua
@@ -0,0 +1,12 @@
+
+-- To load images and sounds that we will use, 1 time
+function love.load()
+end
+
+-- All the logic's game, 60 times per second
+function love.update() 
+end
+
+-- To draw all the game, 60 times per second
+function love.draw()
+end
