f09396db6ce008163a71bcd87285d6d9a4ce57f1
Mouaad MAIOUF <mouaad69@hotmail.fr>
2022-10-29 21:06:49 +0200

    added the ball + her speed

--- Project/main.lua
+++ Project/main.lua
@@ -1,3 +1,4 @@
+widthScreen, heightScreen = love.graphics.getDimensions()
 
 racket = {}
 racket.x = 0
@@ -5,10 +6,18 @@ racket.y = 0
 racket.width = 20
 racket.height = 80
 
-widthScreen, heightScreen = love.graphics.getDimensions()
+ball = {}
+ball.x = widthScreen / 2
+ball.y = heightScreen / 2
+ball.width = 20
+ball.height = 20
+ball.speed_x = 2
+ball.speed_y = -2
 
 -- To load images and sounds that we will use, 1 time
 function love.load()
+  ball.x = ball.x - (ball.width/2)
+  ball.y = ball.y - (ball.height/2)
 end
 
 -- All the logic's game, 60 times per second
@@ -22,10 +31,14 @@ function love.update()
     if love.keyboard.isDown("up") and racket.y > 0 then
       racket.y = racket.y - 2
     end
-
+    
+  ball.x = ball.x + ball.speed_x
+  ball.y = ball.y + ball.speed_y
+  
 end
 
 -- To draw all the game, 60 times per second
 function love.draw()
   love.graphics.rectangle("fill", racket.x, racket.y, racket.width, racket.height)
+  love.graphics.rectangle("fill", ball.x, ball.y, ball.width, ball.height)
 end
